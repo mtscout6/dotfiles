94b199ada4907d56cf0b650960c9eb1c108bf7c8
Matthew Smith <mtscout6@gmail.com>
2015-09-10 21:16:33 -0600

    Add install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Make symbolic links to home folder
+link_file() {
+  source="${PWD}/$1"
+  target="${HOME}/${1/_/.}"
+
+  if [ -e $target ] ; then
+    if [ ! -d $target ] ; then
+      echo "Update\t$target"
+      mv $target $target.bak
+      ln -sf ${source} ${target}
+    fi
+  else
+    echo "Install\t$target"
+    ln -sf ${source} ${target}
+  fi
+}
+
+for i in _*
+do
+  link_file $i
+done
+link_file bin
+
+# Vim Package Manager Install
+if [ ! -d $HOME/.vim/bundle/neobundle.vim ]
+then
+  git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+fi
